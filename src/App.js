3f7c0ef0163d027eb464509fa6ab2a433d33d8f7
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-07-29 15:33:04 -0400

    finished RPS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import PlayerCard from './PlayerCard.js'
+
+class Game extends Component {
+  constructor(){
+    super()
+    this.signs = ['rock', 'paper', 'scissors']
+    this.state = {
+      playerOne: 'rock',
+      playerTwo: 'scissors'
+    }
+  }
+
+  playGame = () => {
+    this.setState({
+      playerOne: this.signs[Math.floor(Math.random() * 3)],
+      playerTwo: this.signs[Math.floor(Math.random() * 3)]
+    })
+  }
+
+  decideWinner = () => {
+    const p1 = this.state.playerOne
+    const p2 = this.state.playerTwo
+     if (p1 === p2) {
+       return 'Tie!'
+     } else if ((p1 === 'rock' && p2 === 'scissors') ||
+                (p1 === 'scissors' && p2 === 'paper')||
+                (p1 === 'paper' && p2 === 'rock')) {
+        return 'Player 1 Wins!!'            
+      } else {
+        return 'Player 2 Wins!!'
+      }
+  }
+
+  render() {
+    return (
+      <div className="style">
+        <div className='players'>
+          <PlayerCard sign={this.state.playerOne}/>
+          <PlayerCard sign={this.state.playerTwo}/>
+        </div>
+        <div className='winner'>{this.decideWinner()}</div>
+        <button type='button' onClick={this.playGame}>Play The Game</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Game
